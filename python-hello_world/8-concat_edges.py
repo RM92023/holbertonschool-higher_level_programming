d1da00ae264e2ba8a6bcae1d1a5ddb8661713088
RM92023 <robinjara20@gmail.com>
2023-05-30 15:11:56 -0500

    Delete spaces in lets

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -2,4 +2,4 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 separator = str.split(' ')
-print(separator[5] + ' ' + separator[6] + ' ' + separator[12] + ' ' + separator[0])
+print(separator[5]+' '+separator[6]+' '+separator[12]+' '+separator[0])
