49645d15d746a6f873ecb8b1965035943c46b745
RM92023 <robinjara20@gmail.com>
2023-05-31 16:23:47 -0500

    update code for add op, ternario

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,10 +2,9 @@
 import random
 number = random.randint(-10000, 10000)
 lastDigit = abs(number) % 10
-print(f"Last digit of {number} is {lastDigit} ", end="")
-if lastDigit > 5:
-    print("and is greater than 5")
-elif lastDigit == 0:
-    print("and is 0")
-else:
-    print("and is less than 6 and not 0")
+validation = (
+    "and is greater than 5"
+    if lastDigit > 5
+    else "and is 0"
+    if lastDigit == 0 else "and is less than 6 and not 0")
+print(f"Last digit of {number} is {lastDigit} {validation}")
