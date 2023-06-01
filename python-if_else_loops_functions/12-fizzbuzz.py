708923c6f1a35e8512147a00a51d4ef34aa063ac
RM92023 <robinjara20@gmail.com>
2023-06-01 17:00:17 -0500

    fix error 12-fizzbuzz

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for fizzbuzz in range(1, 100):
+    for fizzbuzz in range(1, 101):
         if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
             print("FizzBuzz", end=' ')
             continue
