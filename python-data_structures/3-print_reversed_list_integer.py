f0b38043ba40da34fe4ad9e690f46e0a477aed4e
RM92023 <robinjara20@gmail.com>
2023-06-06 09:55:56 -0500

    adding :d in string

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -4,4 +4,4 @@ def print_reversed_list_integer(my_list=[]):
         return
     for i in range(len(my_list)-1, -1, -1):
         if isinstance(my_list[i], int):
-            print('{}'.format(my_list[i]))
+            print('{:d}'.format(my_list[i]))
