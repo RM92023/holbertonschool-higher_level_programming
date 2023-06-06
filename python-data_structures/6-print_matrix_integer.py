444f7dc93500920371bb30726e57028faaf9693e
RM92023 <robinjara20@gmail.com>
2023-06-06 11:04:01 -0500

    update print with format

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for col, element in enumerate(row):
-            print(element, end='')
+            print('{:d}'.format(element), end='')
             if col != len(row)-1:
                 print(' ', end='')
         print()
