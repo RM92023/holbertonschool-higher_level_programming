0248c1874b5fd69b49938c9fd66470d73180766c
RM92023 <robinjara20@gmail.com>
2023-06-08 14:03:00 -0500

    Update file

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -8,25 +8,26 @@ def list_division(my_list_1, my_list_2, list_length):
                 value_1 = my_list_1[i]
                 value_2 = my_list_2[i]
 
-                if not isinstance((value_1, (int, float))
-                                  or not isinstance(value_2, (int, float))):
+                if not isinstance(value_1, (int, float)) or not isinstance(value_2, (int, float)):
                     raise TypeError("wrong type")
 
                 if value_2 == 0:
                     raise ZeroDivisionError("division by 0")
-
+                
                 result = value_1 / value_2
+                result_list.append(result)
+
             except IndexError:
                 print("out of range")
-                result = 0
+                result_list.append(0)
+
             except TypeError:
                 print("wrong type")
-                result = 0
+                result_list.append(0)
+
             except ZeroDivisionError:
                 print("division by 0")
-                result = 0
-            finally:
-                result_list.append(result)
+                result_list.append(0)
 
     finally:
         return result_list
