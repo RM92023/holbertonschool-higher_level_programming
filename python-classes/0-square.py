2cea87986c7df039e05d864e3c8ef9cba77a9fdd
RM92023 <robinjara20@gmail.com>
2023-06-13 09:28:10 -0500

    Adding README.md and 0-square.py

---
+++ python-classes/0-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+define class called Square
+"""
+class Square:
+    def __init__(self):
+        """
+        Initialization method of the Square class.
+        Called automatically when a new object of the Square class is created.
+        """
+        Square.square = None
+        """
+        Sets the "square" attribute of the Square class to None.
+        This attribute is shared by all objects of the class,
+        since you are assigning directly to the class instead of a specific instance.
+        """
