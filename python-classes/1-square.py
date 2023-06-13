acb338e12892ad289dd97b6db2e06ef192a91abb
RM92023 <robinjara20@gmail.com>
2023-06-13 10:19:55 -0500

    Adding 1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Define class Square with Private atribute Size
+"""
+
+
+class Square:
+    """
+    Define Constructor with Private atribute Size
+    __ is the name of the private attribute
+    """
+    def __init__(self, size):
+        self.__size = size
