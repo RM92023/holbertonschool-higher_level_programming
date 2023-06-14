b9ff21a00a4a3fd68788734f9a8356f09bac4c1b
RM92023 <robinjara20@gmail.com>
2023-06-14 16:16:01 -0500

    0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""Define the function to be called add_integer"""
 def add_integer(a, b=98):
+    """Add integer"""
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
