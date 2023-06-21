27c8bd46ac40bbac7925227c45217f1880c305a2
RM92023 <robinjara20@gmail.com>
2023-06-21 14:36:38 -0500

    Update position in super

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -8,8 +8,8 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     def __init__(self, size):
         self.integer_validator("size", size)
-        self.__size = size
         super().__init__(size, size)
+        self.__size = size
 
     def __str__(self):
         return f"[Square] {self.__size}/{self.__size}"
