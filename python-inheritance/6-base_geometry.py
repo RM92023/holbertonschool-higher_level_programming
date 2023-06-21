e8353b9f5ef780c51868a3ced496483f5701e7d5
RM92023 <robinjara20@gmail.com>
2023-06-21 10:41:35 -0500

    Adding area without implement

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Define class empty"""
+
+
+class BaseGeometry:
+    """Class empty it does not nothing"""
+
+    def __init__(self):
+        pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
