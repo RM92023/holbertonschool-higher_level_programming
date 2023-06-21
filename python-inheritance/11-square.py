e6eb012086ba2f5b0e841d549f1ab4e8abf03f8f
RM92023 <robinjara20@gmail.com>
2023-06-21 14:38:53 -0500

    Changing positions

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -7,9 +7,9 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     def __init__(self, size):
+        self._size = size
         self.integer_validator("size", size)
         super().__init__(size, size)
-        self._size = size
 
     def __str__(self):
         return f"[Square] {self._size}/{self._size}"
