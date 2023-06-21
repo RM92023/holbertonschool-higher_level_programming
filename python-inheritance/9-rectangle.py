bba0f9a2b584870e18ed4aa73d449c6aa6116731
RM92023 <robinjara20@gmail.com>
2023-06-21 13:48:48 -0500

    Private class

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -12,11 +12,11 @@ class Rectangle(BaseGeometry):
         super().__init__()
         self.integer_validator("width", width)
         self.integer_validator("height", height)
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
     def area(self):
-        return self.width * self.height
-    
+        return self.__width * self.__height
+
     def __str__(self):
-        return f"[Rectangle] {self.width}/{self.height}"
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
