5b10e75c28d35301042a488965dd373a72875471
RM92023 <robinjara20@gmail.com>
2023-06-21 10:33:12 -0500

    Update styles

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Define inheritance for classes"""
+
+
 def inherits_from(obj, a_class):
+    """define type inheritance"""
     if type(obj) != a_class:
         return issubclass(type(obj), a_class)
     return False
