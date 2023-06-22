196f3c23f5d81bc2bc04406984be2d1a8f1acf5b
RM92023 <robinjara20@gmail.com>
2023-06-22 09:22:08 -0500

    Update format UTF8 to utf-8

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """Define open file"""
-    with open(filename, encoding='UTF8') as f:
+    with open(filename, 'r', encoding='utf-8') as f:
         print(f.read(), end='\n')
