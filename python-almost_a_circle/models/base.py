7f35ea3072707a602b32048cd874ad042b2598f6
RM92023 <robinjara20@gmail.com>
2023-06-26 11:43:31 -0500

    Adding code validation and test in the first file called base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
+"""Define the Base Class"""
+
+
 class Base:
+    """constructor"""
     __nb_objects = 0
 
+    """Function Initialized"""
+
     def __init__(self, id=None):
-        if id is None:
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,3 +1,21 @@
 import unittest
 from models.base import Base
 
+def test_no_arguments(self):
+    b1 = Base()
+    b2 = Base()
+    self.assertEqual(b1.id, b2.id - 1)
+
+def test_three_arguments(self):
+    b1 = Base()
+    b2 = Base()
+    b3 = Base()
+    self.assertEqual(b1.id, b2.id, b3.id - 2)
+
+def test_none_id(self):
+    b1 = Base(None)
+    b2 = Base(None)
+    self.assertEqual(b1.id, b2.id - 1)
+
+def test_unique_id(self):
+    self.assertEqual(20, Base().id)
