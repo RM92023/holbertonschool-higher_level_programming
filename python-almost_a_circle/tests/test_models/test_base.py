0c484508839c60c98670f43862d5e18770f1715f
RM92023 <robinjara20@gmail.com>
2023-06-26 12:43:27 -0500

    update test in only class

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,21 +1,24 @@
 import unittest
 from models.base import Base
 
-def test_no_arguments(self):
-    b1 = Base()
-    b2 = Base()
-    self.assertEqual(b1.id, b2.id - 1)
 
-def test_three_arguments(self):
-    b1 = Base()
-    b2 = Base()
-    b3 = Base()
-    self.assertEqual(b1.id, b2.id, b3.id - 2)
 
-def test_none_id(self):
-    b1 = Base(None)
-    b2 = Base(None)
-    self.assertEqual(b1.id, b2.id - 1)
+class TestBase(unittest.TestCase):
 
-def test_unique_id(self):
-    self.assertEqual(20, Base().id)
+    
+    def multiples_args(self):
+        b1 = Base()
+        b2 = Base()
+        self.assertEqual(b1.id, b2.id - 1)
+
+        b3 = Base()
+        self.assertEqual(b1.id, b2.id, b3.id - 2)
+
+        b1 = Base(None)
+        b2 = Base(None)
+        self.assertEqual(b1.id, b2.id - 1)
+
+        self.assertEqual(20, Base().id)
+
+if __name__ == '__main__':
+    unittest.main()
