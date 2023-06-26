5980aeb4d9a509f7b41c6b8230558f4b9e3edcd9
RM92023 <robinjara20@gmail.com>
2023-06-26 15:35:06 -0500

    update test and code in rectangle

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -18,6 +18,42 @@ class TestRectangle(unittest.TestCase):
 
         self.assertEqual(20, Rectangle().id)
 
+    def test_rectangle_initialization(self):
+        rect = Rectangle(5, 10)
+        self.assertEqual(rect.width, 5)
+        self.assertEqual(rect.height, 10)
+        self.assertEqual(rect.x, 0)
+        self.assertEqual(rect.y, 0)
+        self.assertIsNone(rect.id)
+
+    def test_rectangle_width_validation(self):
+        with self.assertRaises(TypeError):
+            rect = Rectangle("invalid", 10)
+        with self.assertRaises(ValueError):
+            rect = Rectangle(0, 10)
+        with self.assertRaises(ValueError):
+            rect = Rectangle(-5, 10)
+
+    def test_rectangle_height_validation(self):
+        with self.assertRaises(TypeError):
+            rect = Rectangle(5, "invalid")
+        with self.assertRaises(ValueError):
+            rect = Rectangle(5, 0)
+        with self.assertRaises(ValueError):
+            rect = Rectangle(5, -10)
+
+    def test_rectangle_x_validation(self):
+        with self.assertRaises(TypeError):
+            rect = Rectangle(5, 10, "invalid")
+        with self.assertRaises(ValueError):
+            rect = Rectangle(5, 10, -1)
+
+    def test_rectangle_y_validation(self):
+        with self.assertRaises(TypeError):
+            rect = Rectangle(5, 10, 0, "invalid")
+        with self.assertRaises(ValueError):
+            rect = Rectangle(5, 10, 0, -1)
+
 
 if __name__ == '__main__':
     unittest.main()
