0e04918c4195ce97b522a56737760badc21fd74b
RM92023 <robinjara20@gmail.com>
2023-06-27 15:49:21 -0500

    Update and adding new requeriment

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -12,5 +12,18 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
+    """print constructor"""
+
     def __str__(self):
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    """Getter for square"""
+    @property
+    def size(self):
+        return self.width
+
+    """setter for square"""
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -1,6 +1,7 @@
 import unittest
 from models.square import Square
 
+
 class TestSquare(unittest.TestCase):
 
     def test_square_constructor(self):
@@ -18,6 +19,25 @@ class TestSquare(unittest.TestCase):
     def test_square_str(self):
         square = Square(5, 2, 3, 1)
         self.assertEqual(str(square), "[Square] (1) 2/3 - 5")
+
+    def test_square_size_getter(self):
+        square = Square(5)
+        self.assertEqual(square.size, 5)
+
+    def test_square_size_setter(self):
+        square = Square(5)
+        square.size = 7
+        self.assertEqual(square.size, 7)
+        self.assertEqual(square.width, 7)
+        self.assertEqual(square.height, 7)
+
+    def test_square_size_setter_with_validation(self):
+        square = Square(5)
+        with self.assertRaises(ValueError):
+            square.size = -2
+        with self.assertRaises(TypeError):
+            square.size = "invalid"
+
 
 if __name__ == '__main__':
     unittest.main()
