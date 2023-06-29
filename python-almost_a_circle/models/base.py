625b0fc85f32735c979a5604a59f4f41d18feabb
RM92023 <robinjara20@gmail.com>
2023-06-29 09:13:10 -0500

    upadte file base in point 18

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -38,3 +38,14 @@ class Base:
         if json_string is None or len(json_string) == 0:
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        if cls.__name__ == 'Rectangle':
+            dummy = cls(1, 1)
+        elif cls.__name__ == 'Square':
+            dummy = cls(1)
+        else:
+            dummy = None
+        dummy.update(**dictionary)
+        return dummy
